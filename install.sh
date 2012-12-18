ae69a996cc4b4b891bb218c309cd334334ea7aab
Mihai Mocanu <mihai.form@gmail.com>
2012-12-18 16:17:37 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -3,12 +3,17 @@
 
 if [ -d ~/.dotfiles ]; then
     cd ~/.dotfiles
-    git pull
+    git pull origin master
 else
     git clone https://bitbucket.org/mvmocanu/dotfiles.git ~/.dotfiles/
 fi
 
 cd ~/.dotfiles
 make install
+
+git submodule sync
 git submodule init
 git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
