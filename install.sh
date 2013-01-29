93cd2065a104643bd63902abce5f8e80710fc19c
Mihai Mocanu <mihai.form@gmail.com>
2013-01-29 10:55:22 +0200

    Added vundle and some updates to vimrc

--- install.sh
+++ install.sh
@@ -2,18 +2,22 @@
 # wget -qO- https://raw.github.com/mvmocanu/dotfiles/master/install.sh | /bin/bash
 
 if [ -d ~/.dotfiles ]; then
+    cd ~/.dotfiles/.vim/bundle/vundle/
+    git pull origin master
     cd ~/.dotfiles
     git pull origin master
+    sudo pip install pip --upgrade
+    sudo pip install flake8 --upgrade
+    vim -u ~/.dotfiles/vim/vimrc - +BundleInstall! +BundleClean! +qall
 else
     git clone https://github.com/mvmocanu/dotfiles.git ~/.dotfiles/
+    git clone https://github.com/gmarik/vundle.git ~/.dotfiles/vim/bundle/vundle
+
+    cd ~/.dotfiles
+    make install
+
+    sudo pip install pip --upgrade
+    sudo pip install flake8 --upgrade
+    vim -u ~/.dotfiles/vim/vimrc - +BundleInstall +qall
 fi
 
-cd ~/.dotfiles
-make install
-
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
